70bb31b5eb2d09c68c0b345709516c2a96d57810
TharinduDR <rhtdranasinghe@gmail.com>
2020-04-13 18:17:49 +0100

    006: Code for Boosting

--- algo/booster/preboost.py
+++ algo/booster/preboost.py
@@ -6,26 +6,36 @@ import numpy as np
 from examples.common.util.download import download_from_google_drive
 
 
-def prepare_training_file(model_type, model_name, google_drive_file, google_drive_file_id, train_df, config, column_name):
+def prepare_training_file(model_type, model_name, google_drive_file, google_drive_file_id, test_df, config, column_name, train_df=None):
     if google_drive_file:
         download_from_google_drive(google_drive_file_id, model_name)
 
     model = STSTransformerModel(model_type, model_name, num_labels=1,
                                 use_cuda=torch.cuda.is_available(), args=config)
 
-    sentence_list = train_df[column_name].tolist()
+    sentence_list = test_df[column_name].tolist()
     similarity_sentence_list = []
     similarity_list = []
+    quality_list = []
 
     for i in tqdm(range(len(sentence_list))):
         sentence = sentence_list[i]
-        list_2 = sentence_list[:i] + sentence_list[i+1:]
+        if train_df is None:
+            list_2 = sentence_list[:i] + sentence_list[i+1:]
+        else:
+            list_2 = train_df[column_name].tolist()
         temp_test = [[sentence, y] for y in list_2]
         preds, model_outputs = model.predict(temp_test)
         max_ind = np.argmax(preds)
 
         similarity_sentence_list.append(list_2[max_ind])
         similarity_list.append(preds[max_ind])
+
+        print(max_ind)
+
+    for i in tqdm(range(len(similarity_sentence_list))):
+        similarity_sentence = similarity_sentence_list[i]
+        test_df.loc[test_df[column_name] == similarity_sentence, 'A'].iloc[0]
 
 
 
