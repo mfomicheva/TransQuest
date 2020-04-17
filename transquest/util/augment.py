d069205d3f9ce1033ce1799faff522a40b1d0679
TharinduDR <rhtdranasinghe@gmail.com>
2020-04-17 18:15:32 +0100

    007: Code for data augmentation

--- transquest/util/augment.py
+++ transquest/util/augment.py
@@ -52,7 +52,7 @@ def semantic_augmentation(sentence_encoder, files, nmt_training_file, column_nam
 
 def normal_augmentation(nmt_training_file, threshhold):
 
-    cut_nmt_training_file = nmt_training_file.head(int(int(nmt_training_file.shape[0]*threshhold)))
+    cut_nmt_training_file = nmt_training_file.head(7000)
     cut_nmt_training_file["labels"] = 1.0
 
     return cut_nmt_training_file
