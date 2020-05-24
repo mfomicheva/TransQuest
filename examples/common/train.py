7228e9ccbf46ba38ee802806189435805d8fc7de
m_fomicheva <mari.fomicheva@gmail.com>
2020-05-24 15:20:14 +0200

    Extracted training methods Added json configuration Added tests and toy data

--- examples/common/train.py
+++ examples/common/train.py
@@ -1,9 +1,13 @@
 import argparse
 import os
 import shutil
+import json
 
 import numpy as np
 import torch
+
+from multiprocessing import cpu_count
+
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 
@@ -11,83 +15,87 @@ from algo.transformers.evaluation import pearson_corr, spearman_corr
 from algo.transformers.run_model import QuestModel
 from examples.common.util.draw import draw_scatterplot
 
-from examples.common.config.train_config import train_config
-from examples.common.config.train_config import MODEL_TYPE
-from examples.common.config.train_config import MODEL_NAME
-from examples.common.config.train_config import SEED
-
 from examples.common.util.data import read_data_files
 from examples.common.util.normalizer import un_fit
+
+
+def train_model(train_set, config, n_fold=None, inject_features=None, test_size=None, return_model=False):
+    seed = config['SEED'] * n_fold if n_fold else config['SEED']
+    model = QuestModel(
+        config['MODEL_TYPE'], config['MODEL_NAME'], num_labels=1, use_cuda=torch.cuda.is_available(), args=config)
+    if test_size:
+        train_n, eval_df_n = train_test_split(train_set, test_size=test_size, random_state=seed)
+    else:
+        train_n = train_set
+        eval_df_n = None
+    model.train_model(
+        train_n, eval_df=eval_df_n, pearson_corr=pearson_corr, spearman_corr=spearman_corr,
+        mae=mean_absolute_error, model_scores=bool(inject_features)
+    )
+    if return_model:
+        return model
+
+
+def evaluate_model(test_set, config, model=None):
+    if model is None:
+        model = QuestModel(
+            config['MODEL_TYPE'], config['best_model_dir'], num_labels=1, use_cuda=torch.cuda.is_available(),
+            args=config
+        )
+    _, model_outputs, _ = model.eval_model(
+        test_set, pearson_corr=pearson_corr, spearman_corr=spearman_corr, mae=mean_absolute_error
+    )
+    return model_outputs
+
+
+def load_config(args):
+    config = json.load(open(args.config))
+    process_count = cpu_count() - 2 if cpu_count() > 2 else 1
+    config.update({
+        'output_dir': os.path.join(args.output_dir, 'outputs'),
+        'best_model_dir': os.path.join(args.output_dir, 'best_model'),
+        'cache_dir': os.path.join(args.output_dir, 'cache_dir'),
+        'process_count': process_count,
+    })
+    return config
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--train_path')
     parser.add_argument('--test_path')
-    parser.add_argument('--inject_features', nargs='+', default=None)
     parser.add_argument('--output_dir')
+    parser.add_argument('--config')
+    parser.add_argument('--inject_features', nargs='+', default=None)
+    parser.add_argument('--test_size', default=0.1, type=float)
     args = parser.parse_args()
-
-    train_config.update({
-        'output_dir': os.path.join(args.output_dir, 'outputs'),
-        'best_model_dir': os.path.join(args.output_dir, 'best_model'),
-        'cache_dir': os.path.join(args.output_dir, 'cache_dir')
-    })
-
+    config = load_config(args)
     train, test = read_data_files(args.train_path, args.test_path, inject_features=args.inject_features)
-    if train_config['evaluate_during_training']:
-        if train_config['n_fold'] > 1:
-            test_preds = np.zeros((len(test), train_config['n_fold']))
-            print(test_preds.shape)
-            for i in range(train_config['n_fold']):
+    if config['evaluate_during_training']:
+        if config['n_fold'] > 1:
+            test_preds = np.zeros((len(test), config['n_fold']))
+            for i in range(config['n_fold']):
                 print('Training with N folds. Now N is {}'.format(i))
-                if os.path.exists(train_config['output_dir']) and os.path.isdir(train_config['output_dir']):
-                    shutil.rmtree(train_config['output_dir'])
-
-                model = QuestModel(MODEL_TYPE, MODEL_NAME, num_labels=1, use_cuda=torch.cuda.is_available(),
-                                   args=train_config)
-                train_n, eval_df_n = train_test_split(train, test_size=0.1, random_state=SEED * i)
-                print('Created train set with {} examples and eval set with {} examples'.format(len(train), len(eval_df_n)))
-                model.train_model(train_n, eval_df=eval_df_n, pearson_corr=pearson_corr, spearman_corr=spearman_corr,
-                                  mae=mean_absolute_error, model_scores=bool(args.inject_features))
-                model = QuestModel(MODEL_TYPE, train_config['best_model_dir'], num_labels=1,
-                                   use_cuda=torch.cuda.is_available(), args=train_config)
-
-                result, model_outputs, wrong_predictions = model.eval_model(test, pearson_corr=pearson_corr,
-                                                                            spearman_corr=spearman_corr,
-                                                                            mae=mean_absolute_error)
+                if os.path.exists(config['output_dir']) and os.path.isdir(config['output_dir']):
+                    shutil.rmtree(config['output_dir'])
+                train_model(train, config, n_fold=i, inject_features=args.inject_features, test_size=args.test_size)
+                model_outputs = evaluate_model(test, config)
                 test_preds[:, i] = model_outputs
-
             test['predictions'] = test_preds.mean(axis=1)
-
         else:
-            model = QuestModel(MODEL_TYPE, MODEL_NAME, num_labels=1, use_cuda=torch.cuda.is_available(),
-                               args=train_config)
-            train, eval_df = train_test_split(train, test_size=0.1, random_state=SEED)
-            model.train_model(train, eval_df=eval_df, pearson_corr=pearson_corr, spearman_corr=spearman_corr,
-                              mae=mean_absolute_error)
-            model = QuestModel(MODEL_TYPE, train_config['best_model_dir'], num_labels=1,
-                               use_cuda=torch.cuda.is_available(), args=train_config)
-            result, model_outputs, wrong_predictions = model.eval_model(test, pearson_corr=pearson_corr,
-                                                                        spearman_corr=spearman_corr,
-                                                                        mae=mean_absolute_error)
+            train_model(train, config, inject_features=args.inject_features, test_size=args.test_size)
+            model_outputs = evaluate_model(test, config)
             test['predictions'] = model_outputs
     else:
-        model = QuestModel(MODEL_TYPE, MODEL_NAME, num_labels=1, use_cuda=torch.cuda.is_available(), args=train_config)
-        model.train_model(
-            train, pearson_corr=pearson_corr, spearman_corr=spearman_corr, mae=mean_absolute_error,
-            inject_features=args.inject_features
-        )
-        result, model_outputs, wrong_predictions = model.eval_model(test, pearson_corr=pearson_corr,
-                                                                    spearman_corr=spearman_corr,
-                                                                    mae=mean_absolute_error)
+        model = train_model(train, config, inject_features=args.inject_features, return_model=True)
+        model_outputs = evaluate_model(test, config, model=model)
         test['predictions'] = model_outputs
 
     test = un_fit(test, 'labels')
     test = un_fit(test, 'predictions')
     test.to_csv(os.path.join(args.output_dir, 'results.tsv'), header=True, sep='\t', index=False, encoding='utf-8')
     draw_scatterplot(test, 'labels', 'predictions', os.path.join(args.output_dir, 'results.png'),
-                     MODEL_TYPE + ' ' + MODEL_NAME)
+                     config['MODEL_TYPE'] + ' ' + config['MODEL_NAME'])
 
 
 if __name__ == '__main__':
---
+++ tests/test_run_model.py
@@ -0,0 +1,45 @@
+import os
+import unittest
+
+from examples.common.train import train_model
+from examples.common.train import load_config
+from examples.common.util.data import read_data_files
+
+
+test_dir = os.path.dirname(os.path.realpath(__file__))
+data_dir = os.path.join(test_dir, '../data')
+
+
+class Args:
+
+    def __init__(self, config, output_dir):
+        self.output_dir = output_dir
+        self.config = config
+
+
+class TestTrain(unittest.TestCase):
+
+    out_dir = os.path.join(data_dir, 'toy', 'output')
+    train_path = os.path.join(data_dir, 'toy', 'toy.tsv')
+    config_path = os.path.join(data_dir, 'toy', 'toy.json')
+    test_path = train_path
+    args = Args(config_path, out_dir)
+
+    def test_trains_model(self):
+        config = load_config(self.args)
+        train, test = read_data_files(self.train_path, self.test_path)
+        train_model(train, config, test_size=0.5)
+
+    def test_trains_model_with_injected_features(self):
+        config = load_config(self.args)
+        train, test = read_data_files(self.train_path, self.test_path, inject_features=['model_scores'])
+        train_model(train, config, test_size=0.5, inject_features=['model_scores'])
+
+# --output_dir
+# /home/marina/Workspace/experiments/TransQuest/models/toy
+# --train_path
+# /home/marina/Workspace/experiments/TransQuest/data/si-en/toy.tsv
+# --test_path
+# /home/marina/Workspace/experiments/TransQuest/data/si-en/toy.tsv
+# --inject_features
+# model_scores
